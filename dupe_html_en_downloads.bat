a048861620469349fa0b25c27b234b27b6f9b899
twente <37577822+twente@users.noreply.github.com>
2021-02-02 14:08:01 +0100

    V0.6 WIP8

---
+++ dupe_html_en_downloads.bat
@@ -0,0 +1,7 @@
+rem https://stackoverflow.com/questions/3058054/create-a-batch-file-to-copy-and-rename-file/3058092
+rem English page names with downloads page, for use with frontend v0.6 WIP8+
+cd covid-prediction
+copy index.html algorithm.html
+copy index.html data.html
+copy index.html about-us.html
+copy index.html downloads.html
