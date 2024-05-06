f24ec8fc92897fedf195345bda8034875b5ef080
twente <37577822+twente@users.noreply.github.com>
2024-05-06 11:46:25 +0200

    archive version testing

--- dupe_html.bat
+++
@@ -1,5 +0,0 @@
-rem https://stackoverflow.com/questions/3058054/create-a-batch-file-to-copy-and-rename-file/3058092
-cd covid-prediction
-copy index.html algoritme.html
-copy index.html data.html
-copy index.html over-ons.html
--- dupe_html_en.bat
+++
@@ -1,6 +0,0 @@
-rem https://stackoverflow.com/questions/3058054/create-a-batch-file-to-copy-and-rename-file/3058092
-rem English page names, for use with frontend v0.4+
-cd covid-prediction
-copy index.html algorithm.html
-copy index.html data.html
-copy index.html about-us.html
